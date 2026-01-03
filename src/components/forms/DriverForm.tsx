09ac9984d2d080723db7f4558e27fc0d35118d61
Om Patil <ompatil2906@gmail.com>
2026-01-03 19:25:12 +0530

    `Added DriverForm component with form fields and submit button`

---
+++ src/components/forms/DriverForm.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import TextInput from "./common/TextInput";
+import SelectInput from "./common/SelectInput";
+
+export default function DriverForm() {
+  return (
+    <form className="space-y-4">
+      <TextInput label="Driver Name" name="name" />
+      <TextInput label="Phone Number" name="phone" />
+
+      <SelectInput
+        label="Availability"
+        name="available"
+        options={[
+          { label: "Available", value: "true" },
+          { label: "Unavailable", value: "false" },
+        ]}
+      />
+
+      <button className="bg-indigo-600 text-white px-4 py-2 rounded-md">
+        Save Driver
+      </button>
+    </form>
+  );
+}
