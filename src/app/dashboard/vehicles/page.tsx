bb7feb05d9c9032342349d431cf5c6247d997f54
Om Patil <ompatil2906@gmail.com>
2026-01-04 11:11:24 +0530

    `Added VehiclesPage component with add vehicle functionality`

---
+++ src/app/dashboard/vehicles/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import VehicleForm from "../../../components/forms/VehicleForm";
+import Modal from "../../../components/modals/Modal";
+
+export default function VehiclesPage() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="p-6">
+      <div className="flex justify-between mb-4">
+        <h1 className="text-2xl font-bold">Vehicles</h1>
+        <button
+          onClick={() => setOpen(true)}
+          className="bg-green-600 text-white px-4 py-2 rounded"
+        >
+          + Add Vehicle
+        </button>
+      </div>
+
+      <div className="border rounded p-4 text-gray-500">
+        Vehicle list will be here
+      </div>
+
+      <Modal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        title="Add Vehicle"
+      >
+        <VehicleForm />
+      </Modal>
+    </div>
+  );
+}
