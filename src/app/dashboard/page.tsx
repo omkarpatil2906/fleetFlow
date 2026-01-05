05255589c8b125f86937a52cea689f428ecb68ba
Om Patil <ompatil2906@gmail.com>
2026-01-05 20:24:24 +0530

    `Added Sidebar component to DashboardPage and removed Tailwind CSS base and components imports`

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "@/components/sidebar/Sidebar";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <Sidebar/>
+    </div>
+  );
+}
