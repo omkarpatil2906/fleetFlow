4a872bdcb60b536b67106627bfdd2a9572d243e3
Om Patil <ompatil2906@gmail.com>
2026-01-07 18:49:19 +0530

    `Refactor BillingForm component to use react-hook-form library`

--- src/components/forms/BillingForm.tsx
+++ src/components/forms/BillingForm.tsx
@@ -1,15 +1,54 @@
 "use client";
 
-import TextInput from "./common/TextInput";
+import { useForm } from "react-hook-form";
+import InputField from "./common/InputFiled";
+
+type BillingFormValues = {
+  amount: number;
+  dueDate: string;
+};
 
 export default function BillingForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<BillingFormValues>({
+    defaultValues: {
+      amount: 0,
+      dueDate: "",
+    },
+  });
+
+  const onSubmit = (data: BillingFormValues) => {
+    console.log("Billing Data:", data);
+  };
+
   return (
-    <form className="space-y-4">
-      <TextInput label="Invoice Amount" name="amount" type="number" />
-      <TextInput label="Due Date" name="dueDate" type="date" />
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <InputField
+        label="Invoice Amount"
+        name="amount"
+        type="number"
+        control={control}
+        error={errors.amount}
+        isMandatory
+      />
+
+      <InputField
+        label="Due Date"
+        name="dueDate"
+        type="date"
+        control={control}
+        error={errors.dueDate}
+        isMandatory
+      />
 
       <div className="flex justify-end">
-        <button className="bg-purple-600  text-white  px-4 py-2 rounded-md">
+        <button
+          type="submit"
+          className="bg-purple-600 text-white px-4 py-2 rounded-md"
+        >
           Generate Invoice
         </button>
       </div>
